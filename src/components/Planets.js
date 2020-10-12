3cc832ae749a11dda1396e32b4dffa22c86233bf
mireiaGomariz <mireiapgomariz@gmail.com>
2020-10-12 18:22:46 +0200

    query variables

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet'
 
-const fetchPlanets = async () => {
-    const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (key, page) => {
+    const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
     return res.json();   
 }
 const Planets = () => {
-    const { data, status } = useQuery('planets', fetchPlanets, {
-        staleTime: 0,
-        cacheTime: 0,
-        onSuccess: () => console.log("data fetch")
-    })
+    const [page, setPage] = useState(1)
+    const { data, status } = useQuery(['planets', page], fetchPlanets)
 
     return ( 
         <div>
             <h2>Planets</h2>
+            <button onClick={() => setPage(1)}>Page 1</button>
+            <button onClick={() => setPage(2)}>Page 2</button>
+            <button onClick={() => setPage(3)}>Page 3</button>
+
             {status === 'error' && (
                 <div>Error fetching data</div>
             )}

fc9ab92a5b8b42b7b95f715a6fcc1288cf5efe7a
mireiaGomariz <mireiapgomariz@gmail.com>
2020-10-12 17:35:30 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Planets from './components/Planet';
+import People from './components/People';
 
 function App() {
+  const [page, setPage] = useState('planets');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars Info</h1>
+      <Navbar setPage={setPage}/>
+      <div className="content">
+        { page === 'planets' ? <Planets /> : <People />}
+      </div>
     </div>
   );
 }
---
+++ src/components/Planet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Planets = () => {
+    return ( 
+        <div>
+            <h2>Planets</h2>
+        </div>
+     );
+}
+ 
+export default Planets;
